f73e96ae934695e866aed563821710b8401e135d
Outer Cloud Studio <outercloudstudio@gmail.com>
2025-08-14 07:50:42 -0500

    Testing execution in main

--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,8 +1,20 @@
 import chisel3._
 import chisel3.util._
+import chiseltest._
+import chiseltest.RawTester.test
 import _root_.circt.stage.ChiselStage
 
 object Main extends App {
     emitVerilog(new Alu(), Array("--target-dir", "generated"));
     emitVerilog(new Lsu(), Array("--target-dir", "generated"));
+
+    test(new Lsu) { dut =>
+        dut.io.write.poke(true.B);
+        dut.io.address.poke(1.U);
+        dut.io.data.poke(2.U);
+
+        dut.clock.step(1);
+
+        println(dut.io.state.peek().litValue);
+    }
 }
